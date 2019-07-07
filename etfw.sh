49ac110c5ced1ee1ee2b3ec3b1fd61bf90213218
Asmodian X <asmodianx@users.noreply.github.com>
2019-07-06 21:01:13 -0500

    added prefix to fix ET syntax error
    I suppose I should really use something to strip out their scripting and use my own firewall rules.  et's script assumes its on an edge device defending against external attackers where really we flat out dont want this coming in or going out.

--- etfw.sh
+++ etfw.sh
@@ -2,17 +2,19 @@
 filename=`tempfile`
 tmp=`tempfile`
 
-if curl -f -o $filename https://rules.emergingthreats.net/fwrules/emerging-IPTABLES-ALL.rules ; then
-	echo "#!/bin/bash">>$tmp
-	echo "IPTABLES='iptables '">>$tmp
-	echo "iptables -F ETBLOCKLIST">>$tmp
-	echo "iptables -F LOGNDROP">>$tmp
-	echo "iptables -F">>$tmp
-	cat $filename >> $tmp
-	rm $filename
-	cp $tmp /root/etfw_cache.sh
-	. $tmp
-	rm $tmp
+if curl -f -o $filename https://rules.emergingthreats.net/fwrules/emerging-IPTABLES-AL$
+        echo "#!/bin/bash">>$tmp
+        echo "IPTABLES='iptables '">>$tmp
+        echo "iptables -F ETBLOCKLIST">>$tmp
+        echo "iptables -F LOGNDROP">>$tmp
+        echo "iptables -F">>$tmp
+        echo "iptables -A LOGNDROP -j LOG --log-prefix 'ET BLOCK: '">>$tmp
+        cat $filename >> $tmp
+        rm $filename
+        cp $tmp /root/etfw_cache.sh
+        . $tmp
+        rm $tmp
 else
-	. /root/etfw_cache.sh
+        . /root/etfw_cache.sh
 fi
+
